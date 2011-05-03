92b80ee8e582af0d63a89f4d03d37cf784c7d22a
DeFirence <defirence@defirence.za.net>
2011-05-03 23:36:07 +0200

    1.9 compatibility

--- lib/em-sofa.rb
+++ lib/em-sofa.rb
@@ -3,6 +3,6 @@ module EventMachine
     autoload :Version, 'em-sofa/version'
     autoload :Mapping, 'em-sofa/mapping'
     autoload :TVRage,  'em-sofa/tvrage'
-    require './em-sofa/request'
+    require File.join(File.dirname(__FILE__), 'em-sofa', 'request')
   end
 end
